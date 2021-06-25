4fbd7f38fca7f3d0b45a82a39647e17b72f5fdb9
Anderson Carvalho <asfcarvalho@me.com>
2021-06-25 14:08:26 -0300

    SPM

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["PianoComposer"])
     ],
     dependencies: [
-        .package(name: "FBSDKLoginKit", url: "https://github.com/facebook/facebook-ios-sdk.git", .upToNextMajor(from: "9.0"))
+        .package(name: "Facebook", url: "https://github.com/facebook/facebook-ios-sdk.git", .upToNextMajor(from: "9.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
